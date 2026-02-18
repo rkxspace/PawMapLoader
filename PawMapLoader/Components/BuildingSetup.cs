a3a630fce06ebf405ab3303cb61382fdf15eddfb
Rocky_The_Protogen <acemmerson66+github@gmail.com>
2026-02-17 19:59:27 -0500

    I forgot what I changed this time

---
+++ PawMapLoader/Components/BuildingSetup.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using Il2CppAudio;
+using Il2CppDestructibles;
+using Il2CppScoring;
+using MelonLoader;
+using UnityEngine;
+
+namespace PawMapLoader.Components
+{
+    [RegisterTypeInIl2Cpp]
+    public class BuildingSetup : MonoBehaviour
+    {
+        public float Health = 0f;
+        public List<GameObject> Sections = new List<GameObject>();
+
+        void Awake()
+        {
+            var damageable = gameObject.AddComponent<Damageable>();
+            var building = gameObject.AddComponent<Building>();
+            var dmgeffectsplayer = gameObject.AddComponent<DamageEffectsPlayer>();
+            var dmgtoscore = gameObject.AddComponent<DamageToScore>();
+            var playsoundondamage = gameObject.AddComponent<PlaySoundOnDamage>();
+            
+            damageable.MaxHealth = Health;
+            damageable.Health = Health;
+            
+            Il2CppSystem.Collections.Generic.List<Damageable> cdamageables = new Il2CppSystem.Collections.Generic.List<Damageable>();
+            foreach (GameObject section in Sections)
+            {
+                var childDamageable = section.AddComponent<Damageable>();
+                childDamageable.MaxHealth = Health/Sections.Count;
+                childDamageable.Health = Health/Sections.Count;
+                childDamageable.Parent = damageable;
+                cdamageables.Add(childDamageable);
+            }
+            
+            damageable._children = cdamageables;
+            
+            building.Damageable = damageable;
+            building.DamageEffectsPlayer = dmgeffectsplayer;
+            
+        }
+    }
+}
