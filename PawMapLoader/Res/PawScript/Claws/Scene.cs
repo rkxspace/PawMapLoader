e7ad7840bdd2d75d519be47becbeaf5908f7eddc
Rocky_The_Protogen <acemmerson66+github@gmail.com>
2026-03-13 19:26:40 -0400

    Again with more progress

---
+++ PawMapLoader/Res/PawScript/Claws/Scene.cs
@@ -0,0 +1,13 @@
+using PawMapLoader.Res.PawScript.Json;
+
+namespace PawMapLoader.Res.PawScript.Claws
+{
+    public class Scene
+    {
+        public static void UnityGameObjectToMemory(PawScriptInstruction instruction, ref int instructionSetter,
+            Interpreter interpreter)
+        {
+            
+        }
+    }
+}
--- PawMapLoader/Res/PawScript/Claws/Script.cs
+++ PawMapLoader/Res/PawScript/Claws/Script.cs
@@ -1,3 +1,4 @@
+using System;
 using PawMapLoader.Res.PawScript.Json;
 using PawMapLoader.Res.PawScript.Resolvers;
 
@@ -13,21 +14,30 @@ namespace PawMapLoader.Res.PawScript.Claws
         public static void ConditionalJump(PawScriptInstruction instruction, ref int instructionSetter,
             Interpreter interpreter)
         {
-            var mode = instruction.Arguments[0]??"S";
-            var input1 = instruction.Arguments[1]??null;
-            var input2 = instruction.Arguments[2]??null;
-            var jumpTo = int.TryParse(instruction.Arguments[3], out _) ? int.Parse(instruction.Arguments[3]) : instructionSetter;
+            var mode = instruction.Arguments[0] ?? "";
+            var input1 = instruction.Arguments[1] ?? null;
+            var input2 = instruction.Arguments[2] ?? null;
+            var jumpTo = int.TryParse(instruction.Arguments[3], out _)
+                ? int.Parse(instruction.Arguments[3])
+                : throw new ArgumentNullException();
 
-            if (mode == "S")
-            {
-                if (input1 == input2)
-                {
-                    instructionSetter = jumpTo;
-                }
+            var resolved1 = PointerResolver.ResolvePointer(input1, interpreter);
+            var resolved2 = PointerResolver.ResolvePointer(input2, interpreter);
+            
+            bool result = false;
+            switch (mode) {
+                case "Equals":
+                    result = Equals(resolved1, resolved2); break;
+                case "NotEqual": result = !Equals(resolved1, resolved2);
+                    break;
+                case "Greater": result = Convert.ToDouble(resolved1) > Convert.ToDouble(resolved2);
+                    break;
+                case "Less": result = Convert.ToDouble(resolved1) < Convert.ToDouble(resolved2); break;
+                case "EqualGreater": result = Convert.ToDouble(resolved1) >= Convert.ToDouble(resolved2); break;
+                case "EqualLess": result = Convert.ToDouble(resolved1) <= Convert.ToDouble(resolved2); break;
             }
-            else if (mode == "D") 
-                if (PointerResolver.ResolvePointer(input1, interpreter) == PointerResolver.ResolvePointer(input2, interpreter))
-                    instructionSetter = jumpTo;
+            
+            instructionSetter = result?jumpTo:instructionSetter;
         }
     }
 }
