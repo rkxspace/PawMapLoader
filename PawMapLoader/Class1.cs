f0b4abbdcd51746333af73f9bf933ea53f56462c
rkxspace <rkxspace@gmail.com>
2026-03-30 10:05:53 -0400

    I hate git

--- PawMapLoader/Class1.cs
+++ PawMapLoader/Class1.cs
@@ -1,5 +1,7 @@
 using MelonLoader;
 using PawMapLoader;
+using PawMapLoader.Res.PawScript;
+using UnityEngine;
 
 [assembly: MelonInfo(typeof(MalonInit), "PawMapLoader", "0.0.3", "Rocky Nexit")]
 [assembly: MelonGame("Dare Looks", "Pawperty Damage")]
@@ -9,5 +11,10 @@ namespace PawMapLoader
     public class MalonInit : MelonMod
     {
         public override void OnInitializeMelon() => Init.InitMelon();
+
+        public override void OnUpdate()
+        {
+            PawScriptRegister.lastFrameTime = Time.timeAsDouble;
+        }
     }
 }
--- PawMapLoader/Res/PawScript/Claws/Player.cs
+++ PawMapLoader/Res/PawScript/Claws/Player.cs
@@ -1,5 +1,18 @@
+using Il2CppCharacter;
+using PawMapLoader.Res.PawScript.Json;
+using PawMapLoader.Res.PawScript.Resolvers;
+
 namespace PawMapLoader.Res.PawScript.Claws
 {
     public class Player
-    {}
+    {
+        private static PlayerManager _pm => PlayerManager.Instance;
+
+        public static void GetMainPlayer(PawScriptInstruction instruction, ref int instructionSetter,
+            Interpreter interpreter)
+        {
+            int memAddr = string.IsNullOrEmpty(instruction.Arguments[0]) ? PointerResolver.ResolvePointerAddress(instruction.Arguments[0], interpreter) :-1;
+            interpreter.WriteMemory(PlayerManager.MainPlayer, memAddr);
+        }
+    }
 }
