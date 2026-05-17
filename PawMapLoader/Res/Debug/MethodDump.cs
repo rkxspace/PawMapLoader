fad8c5f41af18ebfe62bff461937e0ed702b2438
rkxspace <rkxspace@gmail.com>
2026-05-16 21:04:11 -0400

    Improve Method Dump

--- PawMapLoader/Res/Debug/MethodDump.cs
+++ PawMapLoader/Res/Debug/MethodDump.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Text;
 using Il2CppSystem;
 using Il2CppSystem.Reflection;
+using Exception = System.Exception;
 
 namespace PawMapLoader.Res.Debug
 {
@@ -11,10 +12,10 @@ namespace PawMapLoader.Res.Debug
         public static void Create()
         {
             var strh_DumpText = new StringBuilder();
+            strh_DumpText.AppendLine("=================== GAME DUMP ===================");
             foreach (var asm in AppDomain.CurrentDomain.GetAssemblies())
             {
-                strh_DumpText.AppendLine("==== GAME DUMP ====");
-                strh_DumpText.AppendLine($"[] {asm.GetName().Name}");
+                strh_DumpText.AppendLine($"[] {asm.GetName().Name} ===================");
                 Type[] types;
                 try {types = asm.GetTypes();} catch { continue; }
 
@@ -22,11 +23,19 @@ namespace PawMapLoader.Res.Debug
                 {
                     strh_DumpText.AppendLine($"|| {type.Name}");
 
-                    foreach (var method in type.GetMethods( BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance))
+                    foreach (var method in type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic |
+                                                           BindingFlags.Static | BindingFlags.Instance |
+                                                           BindingFlags.DeclaredOnly))
                     {
-                        var tempstr = string.Join(", ", method.GetParameters().Select(p => $"{p.ParameterType.Name} {p.Name}"));
-                        strh_DumpText.AppendLine($"||==> {method.Name}({tempstr})");
-                    }
+                        try
+                        {
+                            var tempstr = string.Join(", ", method.GetParameters().Select(p => $"{p.ParameterType.Name} {p.Name}"));
+                            strh_DumpText.AppendLine($"||==> {method.Name}({tempstr}) ==> ret {method.ReturnType.Name}");
+                        }
+                        catch (Exception e)
+                        {
+                            strh_DumpText.AppendLine($"||==> {method.Name}(Unk) ==> ret Unk [error: {e.Message}]");
+                        }                    }
                 }
             }
             File.WriteAllText(Path.Combine(Environment.CurrentDirectory, "GameDump.txt"), strh_DumpText.ToString());
