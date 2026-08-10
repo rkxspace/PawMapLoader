176745ba8b7ea79c28af9ed5708d100ee31502ec
rkxspace <rkxspace@gmail.com>
2026-08-10 15:21:42 -0400

    Build-036 Auto-Commit

--- PawMapLoader/Res/Components/PawScriptHitBoxTrigger.cs
+++ PawMapLoader/Res/Components/PawScriptHitBoxTrigger.cs
@@ -1,10 +1,11 @@
 using System;
 using MelonLoader;
+using UnityEngine;
 
 namespace PawMapLoader.Res.Components
 {
     [RegisterTypeInIl2Cpp]
-    public class PawScriptHitBoxTrigger
+    public class PawScriptHitBoxTrigger : MonoBehaviour //silly me, forgot MonoBehavior...
     {
         private void Awake()
         {
