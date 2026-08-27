d1cf532b49872dd97a31b67b4de07ec1d5fd6af5
rkxspace <rkxspace@gmail.com>
2026-08-27 11:20:07 -0400

    Camera Init Touchups

--- PawMapLoader/Res/RKXBOX_Unity_OSS/Res/Camera.cs
+++ PawMapLoader/Res/RKXBOX_Unity_OSS/Res/Camera.cs
@@ -1,12 +1,21 @@
 namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res
 {
+    using UnityEngine;
+
     public class Camera
     {
         public static UnityEngine.Camera camera;
+        public static RenderTexture renderTexture;
+
         public static void CreateEditorCamera()
         {
-            camera = new UnityEngine.GameObject().AddComponent<UnityEngine.Camera>();
-            camera.transform.Rotate(new UnityEngine.Vector3(25, 225, 0));
+            camera = new GameObject("EditorCamera").AddComponent<UnityEngine.Camera>();
+            camera.transform.SetPositionAndRotation(
+                new Vector3(3, 3, 3),
+                new Quaternion(25, 225, 0, 0)
+            );
+            //renderTexture = new RenderTexture();
+            //renderTexture.
         }
     }
 }
