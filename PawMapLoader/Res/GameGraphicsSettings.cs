a18b182e8a1ddfeaaa8b7d4c203544ede7b8982a
rkxspace <rkxspace@gmail.com>
2026-08-28 08:45:52 -0400

    finish potato setting URP asset

--- PawMapLoader/Res/GameGraphicsSettings.cs
+++ PawMapLoader/Res/GameGraphicsSettings.cs
@@ -3,6 +3,7 @@ namespace PawMapLoader.Res
     //TODO
     using System.Collections.Generic;
     using UnityEngine;
+    using UnityEngine.Rendering;
     using UnityEngine.Rendering.Universal;
 
     public enum QualitySettings
@@ -27,9 +28,19 @@ namespace PawMapLoader.Res
             {
                 UniversalRenderPipelineAsset pipelineAsset =
                     ScriptableObject.CreateInstance<UniversalRenderPipelineAsset>();
+                pipelineAsset.supportsCameraDepthTexture = false;
+                pipelineAsset.supportsCameraOpaqueTexture = false;
+                pipelineAsset.gpuResidentDrawerMode = GPUResidentDrawerMode.Disabled;
                 pipelineAsset.additionalLightsRenderingMode = LightRenderingMode.Disabled;
-                // pipelineAsset.additionalLightsShadowmapResolution = 
-                return null; // compiler satisfaction for now
+                pipelineAsset.additionalLightsShadowResolutionTierHigh = 0;
+                pipelineAsset.additionalLightsShadowResolutionTierMedium = 0;
+                pipelineAsset.additionalLightsShadowResolutionTierLow = 0;
+                pipelineAsset.apvScenesData = null;
+                pipelineAsset.cascade2Split = 0;
+                pipelineAsset.cascade3Split = new Vector2(0, 0);
+                pipelineAsset.cascade4Split = new Vector2(0, 0);
+                pipelineAsset.msaaSampleCount = 0;
+                return pipelineAsset;
             }
         }
     }
