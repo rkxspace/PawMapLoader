82d320d154c27cbca25cf7535bd47de1be7f180a
rkxspace <rkxspace@gmail.com>
2026-08-28 14:29:19 -0400

    Build-069 Auto-Commit

--- PawMapLoader/Res/RKXBOX_Unity_OSS/Res/Camera.cs
+++ PawMapLoader/Res/RKXBOX_Unity_OSS/Res/Camera.cs
@@ -11,14 +11,14 @@ namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res
         {
             camera = new GameObject("EditorSceneCamera").AddComponent<Camera>();
             camera.transform.SetPositionAndRotation(
-                new Vector3(3, 3, 3),
+                new Vector3(8, 5, 8),
                 Quaternion.Euler(25, 225, 0)
             );
-            camera.rect = new Rect(0.3f, 0f, 0.7f, 0.7f);
+            camera.rect = new Rect(0.0f, 0.3f, 0.7f, 0.7f);
+            camera.depth = 0;
 
             UICamera = new GameObject("UICamera").AddComponent<Camera>();
-            UICamera.clearFlags = CameraClearFlags.Depth;
-            UICamera.backgroundColor = Color.black;
+            UICamera.clearFlags = CameraClearFlags.Nothing;
             UICamera.orthographic = true;
             UICamera.depth = 10;
             UICamera.cullingMask = LayerMask.GetMask("UI");
