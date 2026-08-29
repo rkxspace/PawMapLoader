e3d688ef8b2591a51fb6b918ca740f7582ae3e47
rkxspace <rkxspace@gmail.com>
2026-08-29 10:29:50 -0400

    Build-076 Auto-Commit

--- PawMapLoader/Res/RKXBOX_Unity_OSS/Res/UI/EditorUI.cs
+++ PawMapLoader/Res/RKXBOX_Unity_OSS/Res/UI/EditorUI.cs
@@ -10,16 +10,26 @@ namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res.UI
             EditorCanvas edtCanvas = new EditorCanvas();
             BoxPanel BottomBar = new BoxPanel(
                 edtCanvas,
+                "LowerExtentBar",
                 new Rect(0.0f, 0.00f, 1f, 0.03f),
-                new Color(0.08f, 0.08f, 0.08f));
+                new Color(0.03f, 0.03f, 0.03f));
             BoxPanel BottomAlignedBox = new BoxPanel(
                 edtCanvas,
+                "BottomPanel",
                 new Rect(0.0f, 0.03f, 0.75f, 0.27f),
                 new Color(0.08f, 0.08f, 0.08f));
             BoxPanel RightAlignedBox = new BoxPanel(
                 edtCanvas,
+                "RightPanel",
                 new Rect(0.75f, 0.03f, 1f, 1f),
                 new Color(0.08f, 0.08f, 0.08f));
+
+            BoxPanel InnerBoxBottomAlignedBox = new BoxPanel(
+                BottomAlignedBox,
+                "InnerBottomPanel",
+                new Rect(0.0f, 0.03f, 0.75f, 0.27f),
+                new Color(0.1f, 0.1f, 0.1f)
+            );
         }
     }
 }
--- PawMapLoader/Res/RKXBOX_Unity_OSS/Res/UI/uGUI/BoxPanel.cs
+++ PawMapLoader/Res/RKXBOX_Unity_OSS/Res/UI/uGUI/BoxPanel.cs
@@ -7,15 +7,30 @@ namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res.UI.uGUI
     {
         public RectTransform rectTransform;
 
-        public BoxPanel(EditorCanvas editorCanvas, Rect ScreenSpace, Color colour)
+        public BoxPanel(EditorCanvas editorCanvas, string name, Rect screenSpace, Color colour)
         {
-            GameObject bxpnl = new GameObject("BoxPanel");
+            GameObject bxpnl = new GameObject($"RootBoxPanel-{name}");
             bxpnl.transform.SetParent(editorCanvas.Canvas.transform, false);
             Image image = bxpnl.AddComponent<Image>();
             image.color = colour;
             RectTransform rt = bxpnl.GetComponent<RectTransform>();
-            rt.anchorMin = new Vector2(ScreenSpace.x, ScreenSpace.y);
-            rt.anchorMax = new Vector2(ScreenSpace.width, ScreenSpace.height);
+            rt.anchorMin = new Vector2(screenSpace.x, screenSpace.y);
+            rt.anchorMax = new Vector2(screenSpace.width, screenSpace.height);
+            rt.offsetMin = Vector2.zero;
+            rt.offsetMax = Vector2.zero;
+            rectTransform = rt;
+            SetUI(bxpnl);
+        }
+
+        public BoxPanel(BoxPanel UprBxPnl, string name, Rect localSpace, Color colour)
+        {
+            GameObject bxpnl = new GameObject($"InnerBoxPanel-{name}");
+            bxpnl.transform.SetParent(UprBxPnl.rectTransform, false);
+            Image image = bxpnl.AddComponent<Image>();
+            image.color = colour;
+            RectTransform rt = bxpnl.GetComponent<RectTransform>();
+            rt.anchorMin = new Vector2(localSpace.x, localSpace.y);
+            rt.anchorMax = new Vector2(localSpace.width, localSpace.height);
             rt.offsetMin = Vector2.zero;
             rt.offsetMax = Vector2.zero;
             rectTransform = rt;
