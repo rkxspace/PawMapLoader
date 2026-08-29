e92b395392d065873adc4d27fb0438a939a79c7a
rkxspace <rkxspace@gmail.com>
2026-08-29 10:14:14 -0400

    Build-075 Auto-Commit

--- PawMapLoader/Res/RKXBOX_Unity_OSS/Res/Camera.cs
+++ PawMapLoader/Res/RKXBOX_Unity_OSS/Res/Camera.cs
@@ -17,7 +17,7 @@ namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res
                 new Vector3(8, 5, 8),
                 Quaternion.Euler(25, 225, 0)
             );
-            camera.rect = new Rect(0.0f, 0.3f, 0.75f, 0.75f);
+            camera.rect = new Rect(0.0f, 0.25f, 0.75f, 0.75f);
             camera.depth = 0;
             camera.tag = "MainCamera";
         }
--- PawMapLoader/Res/RKXBOX_Unity_OSS/Res/ViewPortControls/MouseTools.cs
+++ PawMapLoader/Res/RKXBOX_Unity_OSS/Res/ViewPortControls/MouseTools.cs
@@ -1,10 +1,12 @@
 namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res.ViewPortControls
 {
     using UnityEngine;
+    using UnityEngine.InputSystem;
+    using UnityEngine.InputSystem.Controls;
 
     public class MouseTools
     {
-        public static Vector2 MousePos => Input.mousePosition;
+        public static Vector2 MousePos => Mouse.current.position.value;
         public static Vector2 NrmlMousePos => new Vector2(MousePos.x/Screen.width, MousePos.y/Screen.height);
         public static bool HoveredViewPort => EditorCameras.camera.rect.Contains(NrmlMousePos);
         public static Vector2 ScaledMousePos => new Vector2(
