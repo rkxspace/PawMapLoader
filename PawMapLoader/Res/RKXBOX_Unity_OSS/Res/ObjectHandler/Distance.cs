d5b0e9e311b25168f0bd471e06d2091a9f23f7cf
rkxspace <rkxspace@gmail.com>
2026-08-30 23:08:49 -0400

    Add distance for vector2

--- PawMapLoader/Res/RKXBOX_Unity_OSS/Res/ObjectHandler/Distance.cs
+++ PawMapLoader/Res/RKXBOX_Unity_OSS/Res/ObjectHandler/Distance.cs
@@ -7,5 +7,8 @@ namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res.ObjectHandler
     {
         public static double DistanceBetween(Vector3 a, Vector3 b) =>
             Math.Sqrt(Math.Pow(b.x - a.x, 2) + Math.Pow(b.z - a.z, 2) + Math.Pow(b.y - a.y, 2));
+
+        public static double DistanceBetween(Vector2 a, Vector2 b) =>
+            Math.Sqrt(Math.Pow(b.x - a.x, 2) + Math.Pow(b.y - a.y, 2));
     }
 }
