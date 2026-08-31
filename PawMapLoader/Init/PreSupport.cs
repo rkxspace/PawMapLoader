757867dda836ec1abeb87496da9a7707d9c9bcda
rkxspace <rkxspace@gmail.com>
2026-08-30 23:13:20 -0400

    add presupport entry

---
+++ PawMapLoader/Init/PreSupport.cs
@@ -0,0 +1,12 @@
+namespace PawMapLoader
+{
+    using UnityEngine.Rendering;
+
+    public class PreSupport
+    {
+        public static void PSup()
+        {
+            SplashScreen.Begin();
+        }
+    }
+}
