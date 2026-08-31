07083d4cf4b4a9340e6a5905d44d2c33d25f70c3
rkxspace <rkxspace@gmail.com>
2026-08-30 23:09:01 -0400

    Add testing stuff

--- PawMapLoader/Init/EarlyInitMelon.cs
+++ PawMapLoader/Init/EarlyInitMelon.cs
@@ -1,13 +1,19 @@
-using PawMapLoader.Res;
-
 namespace PawMapLoader
 {
+    using MelonLoader;
+    using Res;
     using Res.GUI;
+    using UnityEngine.Rendering;
 
     public class EarlyInitMelon
     {
         public static void EarlyInit()
         {
+            PreSupport.PSup();
+            Store.InitScnevnt += (i, a) => { MelonLogger.Msg($"TESTING: {a}, {i}"); };
+
+            MelonLogger.Msg($"MDN: {SplashScreen.BeginDelegateField.Method.GetType().FullName}" +
+                            $" {SplashScreen.BeginDelegateField.Method.Name}");
             Strings.ValidateLocaleExist();
             NTCheck.WineCheck();
             EditorEntryButton.Register();
