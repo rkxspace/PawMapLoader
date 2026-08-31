4b5a4fc7027192b0f1af7067c269a43d728fcc7f
rkxspace <rkxspace@gmail.com>
2026-08-30 23:08:40 -0400

    Fix up debugger to make it more usable

--- PawMapLoader/Res/RKXBOX_Unity_OSS/Res/Components/Debugger.cs
+++ PawMapLoader/Res/RKXBOX_Unity_OSS/Res/Components/Debugger.cs
@@ -1,6 +1,7 @@
 namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res.Components
 {
     using MelonLoader;
+    using UI.uGUI;
     using UnityEngine;
     using ViewPortControls;
     using TextMesh = UI.uGUI.TextMesh;
@@ -8,10 +9,17 @@ namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res.Components
     [RegisterTypeInIl2Cpp]
     public class Debugger : MonoBehaviour
     {
+        public BoxPanel BoxPanel;
         public TextMesh Text;
 
         private void Update()
         {
+            BoxPanel.UpdateRect(new Rect(
+                Mathf.Clamp(MouseTools.NrmlMousePos.x + 0.01f, 0.01f, 0.79f),
+                Mathf.Clamp(MouseTools.NrmlMousePos.y + 0.01f, 0.03f, 0.89f),
+                Mathf.Clamp(MouseTools.NrmlMousePos.x + 0.21f, 0.22f, 1f),
+                Mathf.Clamp(MouseTools.NrmlMousePos.y + 0.11f, 0.14f, 1f)
+            ));
             Text.textMesh.text = $"MOUSE POS: {(MouseTools.HoveredViewPort ? "VPHOV" : "NHOV")} // " +
                                  $"{MouseTools.NrmlMousePos.x}|{MouseTools.NrmlMousePos.y}" +
                                  $"\nHOVEROBJ: {MouseTools.GetHoveredGameObject()?.name}";
