1467bc71dc0e4476f8d26c53a5e37e14372a4973
rkxspace <rkxspace@gmail.com>
2026-08-30 23:08:14 -0400

    Add Original Rotation + change rect to be new editor camerarect var

--- PawMapLoader/Res/RKXBOX_Unity_OSS/Res/Camera.cs
+++ PawMapLoader/Res/RKXBOX_Unity_OSS/Res/Camera.cs
@@ -7,6 +7,7 @@ namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res
     public class EditorCameras
     {
         public static Camera camera;
+        public static Vector3 OriginalRot = Vector3.zero;
 
         public static void CreateEditorCamera()
         {
@@ -17,7 +18,7 @@ namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res
                 new Vector3(8, 5, 8),
                 Quaternion.Euler(25, 225, 0)
             );
-            camera.rect = new Rect(0.0f, 0.25f, 0.75f, 0.75f);
+            camera.rect = EditorStates.instance.cameraRect;
             camera.depth = 0;
             camera.tag = "MainCamera";
         }
