a2ddb97f543b06642951c6c6e9768585018fc126
rkxspace <rkxspace@gmail.com>
2026-08-30 23:09:26 -0400

    Add camera rect setter and getter along with editor cursor state

--- PawMapLoader/Res/RKXBOX_Unity_OSS/Res/EditorStates.cs
+++ PawMapLoader/Res/RKXBOX_Unity_OSS/Res/EditorStates.cs
@@ -5,12 +5,24 @@ namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res
     public class EditorStates
     {
         public static EditorStates instance;
+        private Rect _cameraRect = new Rect(0.0f, 0.25f, 0.75f, 0.75f);
+        public CursorLockMode cursorLockMode = CursorLockMode.None;
+
+        public GameObject selectedGameObject = null;
+
+        public Rect cameraRect
+        {
+            get => _cameraRect;
+            set
+            {
+                _cameraRect = value;
+                EditorCameras.camera.rect = _cameraRect;
+            }
+        }
 
         public static void StateSetup()
         {
             instance = new EditorStates();
         }
-        
-        public GameObject selectedGameObject = null;
     }
 }
