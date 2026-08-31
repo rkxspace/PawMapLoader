781af735a36064afdbb942222fd3033ae285c3dd
rkxspace <rkxspace@gmail.com>
2026-08-30 23:13:41 -0400

    Add highlight object field

--- PawMapLoader/Res/RKXBOX_Unity_OSS/Res/ObjectHandler/Data/SceneObj.cs
+++ PawMapLoader/Res/RKXBOX_Unity_OSS/Res/ObjectHandler/Data/SceneObj.cs
@@ -1,10 +1,11 @@
-namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res.ObjectHandler
+namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res.ObjectHandler.Data
 {
     using UnityEngine;
 
     public class SceneObj
     {
         public GameObject GameObject;
+        public SceneObjHighlight Highlight;
         public Renderer Renderer;
     }
 }
