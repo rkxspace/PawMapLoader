720544791e8f8cb72579d2b2b14f4a4df0447a19
rkxspace <rkxspace@gmail.com>
2026-08-30 23:11:10 -0400

    Move setpass, set rect for GL rendering, change zwrite to 1

--- PawMapLoader/Res/RKXBOX_Unity_OSS/Res/UI/GLHelper.cs
+++ PawMapLoader/Res/RKXBOX_Unity_OSS/Res/UI/GLHelper.cs
@@ -1,6 +1,7 @@
 namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res.UI
 {
     using UnityEngine;
+    using UnityEngine.Rendering;
 
     public class GLHelper
     {
@@ -12,35 +13,38 @@ namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res.UI
             {
                 Shader shdr = Shader.Find("Hidden/Internal-Colored");
                 _colourMaterial = new Material(shdr);
+                _colourMaterial.shader = shdr;
                 _colourMaterial.hideFlags = HideFlags.HideAndDontSave;
-                _colourMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-                _colourMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.One);
-                _colourMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
-                _colourMaterial.SetInt("_ZWrite", 0);
-                _colourMaterial.SetPass(0);
+                _colourMaterial.SetInt("_SrcBlend", (int)BlendMode.One);
+                _colourMaterial.SetInt("_DstBlend", (int)BlendMode.One);
+                _colourMaterial.SetInt("_Cull", (int)CullMode.Off);
+                _colourMaterial.SetInt("_ZWrite", 1);
             }
         }
 
         public static void Render(Transform transform)
         {
             GLInit();
-            
+
             GL.PushMatrix();
+            GL.Viewport(EditorCameras.camera.rect);
+            GL.LoadProjectionMatrix(EditorCameras.camera.projectionMatrix);
             GL.MultMatrix(transform.localToWorldMatrix);
+            _colourMaterial.SetPass(0);
             GL.Begin(GL.LINES);
-            
+
             GL.Color(new Color(0.7f, 0, 0));
             GL.Vertex3(0, 0, 0);
             GL.Vertex3(1, 0, 0);
-            
+
             GL.Color(new Color(0, 0.7f, 0));
             GL.Vertex3(0, 0, 0);
             GL.Vertex3(0, 1, 0);
-            
+
             GL.Color(new Color(0, 0, 0.7f));
             GL.Vertex3(0, 0, 0);
             GL.Vertex3(0, 0, 1);
-            
+
             GL.End();
             GL.PopMatrix();
         }
