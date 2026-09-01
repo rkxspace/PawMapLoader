6983b20c2acb06d226a1dd1478ab0d275dc308f1
rkxspace <rkxspace@gmail.com>
2026-08-31 23:15:42 -0400

    Add new gameobject add function to PML_SCENE

--- PawMapLoader/Res/RKXBOX_Unity_OSS/Res/ObjectHandler/Data/PML_Scene.cs
+++ PawMapLoader/Res/RKXBOX_Unity_OSS/Res/ObjectHandler/Data/PML_Scene.cs
@@ -1,6 +1,7 @@
 namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res.ObjectHandler.Data
 {
     using System.Collections.Generic;
+    using UnityEngine;
 
     public class PML_Scene
     {
@@ -8,5 +9,16 @@ namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res.ObjectHandler.Data
         public Dictionary<string, SceneObj> sceneData = new Dictionary<string, SceneObj>();
 
         public PML_Scene() => Instance = this;
+
+        public void AddGameObject(GameObject gameObject)
+        {
+            sceneData.Add(gameObject.name,
+                new SceneObj
+                {
+                    GameObject = gameObject,
+                    Renderer = gameObject.GetComponent<Renderer>()
+                }
+            );
+        }
     }
 }
