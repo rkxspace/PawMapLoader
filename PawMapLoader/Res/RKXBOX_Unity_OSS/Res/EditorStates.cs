32b9e5ad95776b246270500e6ce482492a6eb478
rkxspace <rkxspace@gmail.com>
2026-08-31 23:13:13 -0400

    Add random and GUID editor values

--- PawMapLoader/Res/RKXBOX_Unity_OSS/Res/EditorStates.cs
+++ PawMapLoader/Res/RKXBOX_Unity_OSS/Res/EditorStates.cs
@@ -1,14 +1,20 @@
 namespace PawMapLoader.Res.RKXBOX_Unity_OSS.Res
 {
+    using System;
     using UnityEngine;
+    using Random = System.Random;
 
     public class EditorStates
     {
         public static EditorStates instance;
-        private Rect _cameraRect = new Rect(0.0f, 0.25f, 0.75f, 0.75f);
+        private Rect _cameraRect = new Rect(0.0f, 0.27f, 0.75f, 0.73f);
         public CursorLockMode cursorLockMode = CursorLockMode.None;
 
+        public Random globalRandom = new Random();
+
         public GameObject selectedGameObject = null;
+
+        public Guid getGuid => Guid.NewGuid();
 
         public Rect cameraRect
         {
